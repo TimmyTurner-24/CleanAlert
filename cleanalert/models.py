c5cd9a42fe6b67971bdd705e39a59b713fa4fbd7
CleanAlert Dev <dev@cleanalert.local>
2026-08-24 08:04:46 +0100

    No commit msg

--- cleanalert/models.py
+++ cleanalert/models.py
@@ -1,9 +1,4 @@
 from datetime import datetime, timezone
-<<<<<<< HEAD
-from itsdangerous import URLSafeTimedSerializer as Serializer
-=======
-# from itsdangerous import URLSafeTimedSerializer as Serializer
->>>>>>> 9ae30c1 (Getting ready for frontend)
 from . import db, login_manager, app
 from flask_login import UserMixin
 
@@ -24,34 +19,10 @@ class User(db.Model, UserMixin):
     role = db.Column(db.String(10), nullable=False, default='resident')
     reports = db.relationship('Report', backref='author', lazy=True)
     
-<<<<<<< HEAD
     def get_reset_token(self, expire_sec=600):
         s = Serializer(app.config['SECRET_KEY'])
         return s.dumps({'user_id': self.id})
     
-    @staticmethod
-    def verify_reset_token(token, expire_sec=600):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            user_id = s.loads(token, max_age=expire_sec)
-        except:
-            return None
-        return User.query.get(user_id)
-=======
-    # def get_reset_token(self, expire_sec=600):
-    #     s = Serializer(app.config['SECRET_KEY'])
-    #     return s.dumps({'user_id': self.id})
-    
-    # @staticmethod
-    # def verify_reset_token(token, expire_sec=600):
-    #     s = Serializer(app.config['SECRET_KEY'])
-    #     try:
-    #         user_id = s.loads(token, max_age=expire_sec)
-    #     except:
-    #         return None
-    #     return User.query.get(user_id)
->>>>>>> 9ae30c1 (Getting ready for frontend)
-
     def __repr__(self):
         return f"User('{self.name}', '{self.email}', '{self.img}', '{self.role}')"
 
--- cleanalert/routes.py
+++ cleanalert/routes.py
@@ -1,14 +1,8 @@
 import os
 from flask import render_template, url_for, redirect, flash, request, abort
-<<<<<<< HEAD
-from .utils import save_picture, admin_required, resident_required, send_reset_email
-from . import app, db
-from .forms import LoginForm, RegistrationForm,UpdateAccountForm, ReportForm, UpdateReportStatus, RequestResetForm, ResetPasswordForm
-=======
 from .utils import save_picture, admin_required, resident_required
 from . import app, db
 from .forms import LoginForm, RegistrationForm,UpdateAccountForm, ReportForm, UpdateReportStatus
->>>>>>> 9ae30c1 (Getting ready for frontend)
 from .models import User, Report
 from werkzeug.security import generate_password_hash as gph, check_password_hash as cph
 from flask_login import login_user, current_user, logout_user, login_required
@@ -43,8 +37,6 @@ def sign_in():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
-<<<<<<< HEAD
-=======
 
 @app.route("/dashboard")
 @login_required
@@ -76,7 +68,6 @@ def signup():
         return redirect(url_for('sign_in'))
     return render_template('register.html', title='Register', form=form, legend='Register Now')
 
->>>>>>> 9ae30c1 (Getting ready for frontend)
 
 
 @app.route("/logout")
@@ -84,28 +75,6 @@ def logout():
     logout_user()
     return redirect(url_for('homepage'))
 
-@app.route("/reset_password", methods=['POST', 'GET'])
-def reset_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
-    form = RequestResetForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        send_reset_email(user)
-        flash('An email has been sent with the code.', 'info')
-        return redirect(url_for('sign_in'))
-    return render_template('reset_request.html', title='Reset Password', form=form)
-
-@app.route("/reset_password/<token>", methods=['POST', 'GET'])
-def reset_token(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
-    user = User.verify_reset_token(token)
-    if not user:
-        flash('That is an invalid or expired token', 'warning')
-        return redirect(url_for('reset_request'))
-    form = ResetPasswordForm()
-    return render_template('reset_token.html', title='Reset Password', form=form)
 # Resident routes
 
 @app.route("/register", methods=['POST', 'GET']) # Creating an account defaults to Resident
@@ -214,10 +183,6 @@ def delete_report(report_id):
 @admin_required
 def admin_report_view():
     page = request.args.get('page', 1, type=int)
-<<<<<<< HEAD
-    reports = Report.query.order_by(Report.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template('Admin/report_stats.html', title='View all residents reports', reports=reports)
-=======
     status = request.args.get('status', None)  # ← get filter from URL
     if status:
         # Filter by status
@@ -226,7 +191,6 @@ def admin_report_view():
         # Show all reports
         reports = Report.query.order_by(Report.date_posted.desc()).paginate(page=page, per_page=10)
     return render_template('Admin/report_stats.html', title='All Reports', reports=reports, current_status=status)
->>>>>>> 9ae30c1 (Getting ready for frontend)
 
 @app.route("/admin/update-report/<int:report_id>", methods=['GET', 'POST'])
 @admin_required
@@ -234,29 +198,17 @@ def update_status(report_id):
     report = Report.query.get_or_404(report_id)
     form = UpdateReportStatus()
     if form.validate_on_submit():
-<<<<<<< HEAD
-        if report.status == 'in progress' or report.status == 'pending':
-            report.status = form.status.data
-            db.session.commit()
-            flash('The report status has been updated!', 'success')
-            return redirect(url_for('admin_report_view'))
-=======
         report.status = form.status.data
         db.session.commit()
         flash('The report status has been updated!', 'success')
         return redirect(url_for('admin_report_view'))
->>>>>>> 9ae30c1 (Getting ready for frontend)
     return render_template('Admin/update_status_report.html', title='View all residents reports', report=report, form=form)
 
 @app.route("/admin/dashboard")
 @admin_required
 def admin_dashboard():
-<<<<<<< HEAD
-    return render_template('Admin/dashboard.html', title='Admin Dashboard', total_residents=User.query.filter_by(role='resident').count(), total_reports=Report.query.count(), pending_reports=Report.query.filter_by(status='pending').count())
-=======
     return render_template('Admin/dashboard.html',
         title='Admin Dashboard', total_residents=User.query.filter_by(role='resident').count(), total_reports=Report.query.count(), pending=Report.query.filter_by(status='pending').count(), in_progress=Report.query.filter_by(status='in progress').count(), resolved=Report.query.filter_by(status='resolved').count(), declined=Report.query.filter_by(status='declined').count())
->>>>>>> 9ae30c1 (Getting ready for frontend)
 
 @app.route("/admin/new-admin", methods=['GET', 'POST'])
 @admin_required
@@ -264,11 +216,7 @@ def create_admin():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = gph(form.password.data)
-<<<<<<< HEAD
-        user = User(name=form.name.data.upper(), email=form.email.data, password=hashed_password, role='admin')
-=======
         user = User(name=form.name.data, email=form.email.data, password=hashed_password, role='admin')
->>>>>>> 9ae30c1 (Getting ready for frontend)
         db.session.add(user)
         db.session.commit()
         flash('Admin successfully created!', 'success')
