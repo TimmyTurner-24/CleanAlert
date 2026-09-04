83d25bcd8a052f85c67a5242d63d72a706edd371
CleanAlert Dev <dev@cleanalert.local>
2026-09-05 00:50:55 +0100

    No commit message

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from cleanalert import create_app
 
-app = create_app()
+# app = create_app()
+app_debug = create_app()
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', debug=False)
-    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', port=5005, debug=False)
+    app_debug.run(host='0.0.0.0', debug=True)
