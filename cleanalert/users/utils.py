4e919696e3d13259e9ada44f069c0de0498e3b74
CleanAlert Dev <dev@cleanalert.local>
2026-09-12 09:32:52 +0100

    No commit msg

--- cleanalert/users/utils.py
+++ cleanalert/users/utils.py
@@ -10,7 +10,7 @@ def save_picture(form_picture, pic_path):
     random_hex = secrets.token_hex(16)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, pic_path, picture_fn)
+    picture_path = os.path.join(current_app.root_path, pic_path, picture_fn)
     form_picture.save(picture_path)
     
     output_size = (360, 360)
