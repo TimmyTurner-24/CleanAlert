c4d7d67cb936981f1c337ea571f63e5bf7a9db59
TimmyTurner-24 <192466239+TimmyTurner-24@users.noreply.github.com>
2026-09-15 02:32:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from cleanalert import create_app
 
 app = create_app()
-# Preserve compatibility with the previous WSGI entry point.
-app_debug = app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False)
