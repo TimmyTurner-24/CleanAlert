a2e60a4ee54dc2de61cf20c792857eadfa3867a4
TimmyTurner-24 <192466239+TimmyTurner-24@users.noreply.github.com>
2026-09-15 05:57:22 +0100

    Update __init__.py

--- cleanalert/__init__.py
+++ cleanalert/__init__.py
@@ -38,4 +38,7 @@ def create_app(config_class=Config):
     from .commands import register_commands
     register_commands(app)
 
+    with app.app_context():
+        db.create_all()
+
     return app
